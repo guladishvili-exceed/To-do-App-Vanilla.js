3f4f4b8c0bd9d9b552c807d05b7d69d9a226f3e1
guladishvili-exceed <63454784+guladishvili-exceed@users.noreply.github.com>
2020-04-13 12:25:39 +0400

    Update todo.js
    Co-Authored-By: Roman-exceed <roman-exceed@users.noreply.github.com>

--- todo.js
+++ todo.js
@@ -1,23 +1,9 @@
-
-
-
-
-
-var forAdd=document.getElementById('addSmth');
-
-
-function addItem() {
-
-    let newStuff = [];
-
-    let inputValue = document.getElementById('adding').value;
-
-    newStuff.push(inputValue);
-
-    console.log(newStuff);
-
-};
-
-
+function todoList() {
+    let item = document.getElementById('toMakeInput').value
+    let text= document.createTextNode(item)
+    let newItem = document.createElement('li')
+    newItem.appendChild(text)
+    document.getElementById('todoList').appendChild(newItem);
+}
 
 
