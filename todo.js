e2a0d17e42a66471409df3f80a24784305468ba8
guladishvili-exceed <63454784+guladishvili-exceed@users.noreply.github.com>
2020-04-13 11:43:03 +0400

    Update todo.js
    Co-Authored-By: Roman-exceed <roman-exceed@users.noreply.github.com>

---
+++ todo.js
@@ -0,0 +1,24 @@
+
+
+
+let newStuff = [];
+
+let forAdd=document.getElementById('addSmth');
+
+
+function addItem() {
+
+    
+
+    let inputValue = document.getElementById('adding').value;
+
+    newStuff.push(inputValue);
+
+    console.log(newStuff);
+
+};
+
+   
+onclick.forAdd =addItem;
+
+
