a2c92e6bb8c22f2fd8ffb19251a17e98aa32f927
guladishvili-exceed <63454784+guladishvili-exceed@users.noreply.github.com>
2020-04-13 16:07:21 +0400

    Update todo.js
    Co-Authored-By: Roman-exceed <roman-exceed@users.noreply.github.com>

--- todo.js
+++ todo.js
@@ -1,16 +1,80 @@
+
+
 function todoList() {
-    let item = document.getElementById('toMakeInput').value
-    let text= document.createTextNode(item);
-    let newItem = document.createElement('li');
-    let checkBox = document.createElement('input')
-    let newButtons = document.createElement('button');
-    newButtons.setAttribute('type','button');
-    newButtons.setAttribute('value','Delete')
-    checkBox.setAttribute('type','checkbox');
-    newItem.appendChild(newButtons);
-    newItem.appendChild(checkBox);
-    newItem.appendChild(text);
-    document.getElementById('todoList').appendChild(newItem);
-}
+
+	let item    = document.getElementById('todoInput').value
+	
+    let text    = document.createTextNode(item)
+    
+
+    let newItem = document.createElement('li')
+
+    newItem.setAttribute('id','itemtomanipulate')
+    
+    let checkBox=document.createElement('input')
+
+    let edtButton = document.createElement('button')
+
+    edtButton.innerHTML = 'Edit'
+
+    edtButton.addEventListener('click',editItem)
+
+    let clrBtn=document.createElement('button')
+
+    clrBtn.innerHTML = 'Remove'
+
+    clrBtn.setAttribute('id','itemToRemove')
+
+    clrBtn.addEventListener('click',removeItem)
+
+    checkBox.setAttribute('type','checkbox')
+
+    newItem.appendChild(edtButton)
+
+    newItem.appendChild(clrBtn)
+
+    newItem.appendChild(checkBox)
+	
+	newItem.appendChild(text)
+	
+	document.getElementById('todoList').appendChild(newItem) } 
 
 
+   
+
+    function removeItem() {
+        let itemToRemove = document.getElementById('itemtomanipulate');
+        itemToRemove.parentNode.removeChild(itemToRemove);
+    }
+
+
+    function editItem() {
+        let itemToEdit = document.getElementById('itemtomanipulate').value;
+
+
+
+        itemToEdit.innerHTML = edditedItem;
+
+        
+
+        
+    };
+
+
+
+    function blankError() {
+
+        let itemLength = document.getElementById('itemtomanipulate').value;
+
+        if(itemLength.value.lenght == 0) {
+
+            alert('you cant leave this blank')
+
+            return false;
+
+
+
+        }
+
+        
+    }
