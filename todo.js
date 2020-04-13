8c5a4e81ceb7e9efcc6aed6959916b1a109e607a
guladishvili-exceed <63454784+guladishvili-exceed@users.noreply.github.com>
2020-04-13 14:06:22 +0400

    Update todo.js
    Co-Authored-By: Roman-exceed <roman-exceed@users.noreply.github.com>

--- todo.js
+++ todo.js
@@ -1,8 +1,15 @@
 function todoList() {
     let item = document.getElementById('toMakeInput').value
-    let text= document.createTextNode(item)
-    let newItem = document.createElement('li')
-    newItem.appendChild(text)
+    let text= document.createTextNode(item);
+    let newItem = document.createElement('li');
+    let checkBox = document.createElement('input')
+    let newButtons = document.createElement('button');
+    newButtons.setAttribute('type','button');
+    newButtons.setAttribute('value','Delete')
+    checkBox.setAttribute('type','checkbox');
+    newItem.appendChild(newButtons);
+    newItem.appendChild(checkBox);
+    newItem.appendChild(text);
     document.getElementById('todoList').appendChild(newItem);
 }
 
