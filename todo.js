88d2c9f16a8ce8b5db29037257f8b14a4e1f45b9
guladishvili-exceed <63454784+guladishvili-exceed@users.noreply.github.com>
2020-04-16 14:54:18 +0400

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,83 +1,164 @@
-let storage = [];
 
-function addItem() {
-  let item = document.getElementById("todoInput").value;
 
-  let text = document.createTextNode(item);
-  let newText = document.createElement("li");
-  let dltButton = document.createElement("button");
-  let edtButton = document.createElement("button");
-  let submitButton = document.createElement('button')
+var taskInput=document.getElementById("new-task");
+var addButton=document.getElementsByTagName("button")[0];
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");
+var completedTasksHolder=document.getElementById("completed-tasks");
 
-  dltButton.innerText = "Delete";
-  dltButton.addEventListener("click", function () {
-    removeItem();
-  });
 
-  edtButton.innerText = "Edit";
-  edtButton.setAttribute = ('id','editbtn')
-  edtButton.addEventListener("click", function () {
-    newItem();
-  });
 
-  submitButton.innerText = 'Submit Edit'
-  submitButton.addEventListener('click',function(){
-    sbmtEdt();
-  })
+var createNewTaskElement=function(taskString){
 
-  newText.appendChild(text);
-  newText.appendChild(dltButton);
-  newText.appendChild(edtButton);
-  newText.appendChild(submitButton);
-  if (item === "") {
-    alert("Cant be Blank");
+	var listItem=document.createElement("li");
+
+
+	var checkBox=document.createElement("input");
+
+	var label=document.createElement("label");
+
+	var editInput=document.createElement("input");
+
+	var editButton=document.createElement("button");
+
+	var deleteButton=document.createElement("button");
+
+	label.innerText=taskString;
+
+	
+	checkBox.type="checkbox";
+	editInput.type="text";
+
+	editButton.innerText="Edit";
+	editButton.className="edit";
+	deleteButton.innerText="Delete";
+	deleteButton.className="delete";
+
+
+
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(editButton);
+	listItem.appendChild(deleteButton);
+	return listItem;
+}
+
+
+
+var addTask=function(){
+	console.log("Add Task...");
+	
+	var listItem=createNewTaskElement(taskInput.value);
+
+	
+	if(listItem === ''){
+    alert('This field cant be empty')
   } else {
-    document.getElementById("item-list").appendChild(newText);
+    document.getElementById('incomplete-tasks').appendChild(listItem)
   }
-  storage.push(item);
-  console.log(storage);
-  
-  let increments = document.getElementById('item-list')
+	bindTaskEvents(listItem, taskCompleted);
 
-  let li = increments.getElementsByTagName('li')
 
-  for (i=0;i<li.length;i++) {
-    li[i].setAttribute('id',i+1)
-  }
+}
+
+
+var editTask=function(){
+console.log("Edit Task...");
+console.log("Change 'edit' to 'save'");
+
+
+var listItem=this.parentNode;
+
+var editInput=listItem.querySelector('input[type=text]');
+var label=listItem.querySelector("label");
+var containsClass=listItem.classList.contains("editMode");
+
+		if(containsClass){
+
+		
+			label.innerText=editInput.value;
+		}else{
+			editInput.value=label.innerText;
+		}
+
+		
+		listItem.classList.toggle("editMode");
 }
 
 
 
 
 
+var deleteTask=function(){
+		console.log("Delete Task...");
 
-
-
-function clrItem() {
-  let clr = document.getElementById("item-list");
-
-  clr.innerHTML = "";
-}
-
-function removeItem() {
-  let rmvItem = document.getElementById("item-list");
-
-  rmvItem.parentNode.removeChild(rmvItem);
-}
-
-function newItem() {
-  
-
-    document.getElementById('item-list').contentEditable = 'true'
+		var listItem=this.parentNode;
+		var ul=listItem.parentNode;
+	
+		ul.removeChild(listItem);
 
 }
 
-function sbmtEdt() {
-  
 
-  document.getElementById('item-list').contentEditable = 'false'
+
+var taskCompleted=function(){
+		console.log("Complete Task...");
+	
+	
+	var listItem=this.parentNode;
+	completedTasksHolder.appendChild(listItem);
+				bindTaskEvents(listItem, taskIncomplete);
 
 }
+
+
+var taskIncomplete=function(){
+		console.log("Incomplete Task...");
+
+		var listItem=this.parentNode;
+	incompleteTaskHolder.appendChild(listItem);
+			bindTaskEvents(listItem,taskCompleted);
+}
+
+
+
+
+
+addButton.onclick=addTask;
+
+
+
+
+var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
+	console.log("bind list item events");
+
+	var checkBox=taskListItem.querySelector("input[type=checkbox]");
+	var editButton=taskListItem.querySelector("button.edit");
+	var deleteButton=taskListItem.querySelector("button.delete");
+
+
+		
+			editButton.onclick=editTask;
+		
+			deleteButton.onclick=deleteTask;
+		
+			checkBox.onchange=checkBoxEventHandler;
+}
+
+
+	for (var i=0; i<incompleteTaskHolder.children.length;i++){
+
+	
+		bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
+	}
+
+
+
+
+	for (var i=0; i<completedTasksHolder.children.length;i++){
+
+		bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
+	}
 
 
 
